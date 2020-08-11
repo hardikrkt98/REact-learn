b7618a8425d70491198025a1752fd0a38081de2b
hardiksingh98 <hardik.singh@turtlemint.com>
2020-08-11 11:54:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,90 +1,92 @@
-import React, { Component } from "react";
-import "./App.css";
-import Person from "./Person/Person";
+import React, { Component } from 'react';
+import classes from './App.css';
+import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
-  person : [ {
-  'name'  :"hardik",
-  },
-  {
-  'name2' : "tanja"
-  } ,
-],
-  showPerson : true 
-  };
-
-  switchNameHandler = (newName) => {
-    // console.log("was clicked");
-    // this.state.persons[0].name = "hardik";  DONT DO THIS
-       this.setState({'name':newName})
-       console.log(this.state);
-      
-
+    persons: [
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  nameChangeHandler = (event) => {
-    
-     this.setState(
-        {
-          'name' : event.target.value
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    } );
 
-        }   
-     )
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( { persons: persons } );
   }
 
-  togglePersonHandler = () =>
-  {
-    const doesShow = this.state.showPerson;
-    this.setState({showPerson : !doesShow})
+  deletePersonHandler = ( personIndex ) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice( personIndex, 1 );
+    this.setState( { persons: persons } );
+  }
 
-  };
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
 
+  render () {
+    let persons = null;
+    let btnClass = '';
 
- 
-  
-
-  render() {
-   
-
-    let persons = null
-    if(this.state.showPerson)
-    {
-       persons = (
-  
+    if ( this.state.showPersons ) {
+      persons = (
         <div>
-  
-        <Person
-          name={this.state.person[0].name}
-  
-        />
-        <Person
-         name = {this.state.person[1].name2}
-         click = {this.switchNameHandler.bind(this,"SINGH")}
-         nameChange = {this.nameChangeHandler}
-        />
-      </div>
-  
-       );
-   
-  
-  
+          {this.state.persons.map( ( person, index ) => {
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.deletePersonHandler( index )}
+                name={person.name}
+                age={person.age}
+                changed={( event ) => this.nameChangedHandler( event, person.id )} />
+            </ ErrorBoundary>
+          } )}
+        </div>
+      );
+
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if ( this.state.persons.length <= 2 ) {
+      assignedClasses.push( classes.red ); // classes = ['red']
+    }
+    if ( this.state.persons.length <= 1 ) {
+      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
-        <h1>HI, I am react boy : </h1>
-        <p>HELLO MUMBAI</p>
-       
-        <button onClick={this.togglePersonHandler}>HIDE</button>
-    {persons }
-    </div>
-    
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+        <button
+          className={btnClass}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
     );
-    
-
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
-};
-
 
 export default App;
